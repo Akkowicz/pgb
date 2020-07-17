bd32456956e59a4f6ee1f128745c7ed3bd6541b3
Mateusz Przybyłowicz <uamfhq@gmail.com>
2020-07-17 12:53:35 +0200

    Refactor tests - extract parser to fixture

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,47 +3,44 @@ from pgb import cli
 
 url = "postgres://adam@example.com:5432/test_db"
 
-def test_parser_without_driver():
+@pytest.fixture
+def parser():
+    return cli.create_parser()
+
+def test_parser_without_driver(parser):
     """
     Without a specified driver, the parser will exit.
     """
     with pytest.raises(SystemExit):
-        parser = cli.create_parser()
         parser.parse_args([url])
 
-def test_parser_with_driver():
+def test_parser_with_driver(parser):
     """
     The parser will exit if it receives a driver without a destination.
     """
     with pytest.raises(SystemExit):
-        parser = cli.create_parser()
         parser.parse_args([url, "--driver", "local"])
 
-def test_parser_with_unknown_driver():
+def test_parser_with_unknown_driver(parser):
     """
     The parser will exit fi the driver name is unknown.
     """
-    
-    parser = cli.create_parser()
 
     with pytest.raises(SystemExit):
         parser.parse_args([url, '--driver', 'azure', 'destination'])
 
-def test_parser_with_known_drivers():
+def test_parser_with_known_drivers(parser):
     """
     The parser will not exit if the driver name is known.
     """
-    parser = cli.create_parser()
 
     for driver in ['local', 's3']:
         assert parser.parse_args([url, '--driver', driver, 'destination'])
 
-def test_parser_with_driver_and_destination():
+def test_parser_with_driver_and_destination(parser):
     """
     The parser will not exit if it receives a driver and destination.
     """
-    parser = cli.create_parser()
-
     args = parser.parse_args([url, '--driver', 'local', '/some/path'])
 
     assert args.url == url
