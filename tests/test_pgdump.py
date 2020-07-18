239d37b8e36e0640dbebe70c442447e0beb98441
Mateusz Przybyłowicz <uamfhq@gmail.com>
2020-07-18 15:29:42 +0200

    Add mocks of pg_dump

---
+++ tests/test_pgdump.py
@@ -0,0 +1,14 @@
+import pytest
+import subprocess
+
+from pgb import pgdump
+
+url = "postgres://alex:password@example.com:5432/test_db"
+
+def test_dump_calls_pg_dump(mocker):
+    """
+    Utilize pg_dump with the new database URL
+    """
+    mocker.patch('subprocess.Popen')
+    assert pgdump.dump(url)
+    subprocess.Popen.assert_called_with(['pgdump', url], stdout=subprocess.PIPE)
