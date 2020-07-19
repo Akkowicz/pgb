8f476dbe4996e6cca12433930ac8a675ee2e7efc
Mateusz Przybyłowicz <uamfhq@gmail.com>
2020-07-19 15:40:12 +0200

    Improve help and add file name setting

--- src/pgb/cli.py
+++ src/pgb/cli.py
@@ -12,14 +12,16 @@ class DriverAction(Action):
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('url', help="URL of the PostgreSQL database to backup")
-    parser.add_argument('--driver',
+    parser.add_argument('--driver', '-d',
                         help="how and where the backups should be stored",
                         nargs=2,
                         action=DriverAction,
+                        metavar=('driver', 'destination'),
                         required=True)
     return parser
 
 def main():
+    import time
     import boto3
     from pgb import pgdump, storage
 
@@ -27,7 +29,11 @@ def main():
     dump = pgdump.dump(args.url)
     if args.driver == 's3':
         client = boto3.client('s3')
-        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        storage.s3(client, dump.stdout, args.destination, file_name)
+        print(f"Backing database to {args.destination} in S3 as {file_name}.")
     else:
         outfile = open(args.destination, 'wb')
         storage.local(dump.stdout, outfile)
+        print(f"Backing database to {args.destination}.")
