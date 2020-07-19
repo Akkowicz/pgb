fe63ea3e905aa2695ddc1fd69719118056fe72ca
Mateusz Przybyłowicz <uamfhq@gmail.com>
2020-07-19 12:44:23 +0200

    Test handling os errors

--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -12,3 +12,11 @@ def test_dump_calls_pg_dump(mocker):
     mocker.patch('subprocess.Popen')
     assert pgdump.dump(url)
     subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
+
+def test_dump_handles_oserror(mocker):
+    """
+    pgdump.dump returns a reasonable error if pg_dump isn't installed
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError('no such file'))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)
